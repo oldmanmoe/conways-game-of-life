1373a96db80b0ae1e7e479bcba186656a46fbd27
Moises Angeles <moises.ea.camilo@gmail.com>
2026-02-23 12:30:13 -0500

    added grid

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import pygame
 import constants
 
-def main():
+def draw_grid(surface):
     
+    for x in range(0, constants.WIDTH, constants.CELL_SIZE):
+        pygame.draw.line(surface, constants.GRID_LINE_COLOR, (x, 0), (x, constants.HEIGHT))
+    
+    for y in range(0, constants.HEIGHT, constants.CELL_SIZE):
+        pygame.draw.line(surface, constants.GRID_LINE_COLOR, (0, y), (constants.WIDTH, y))
+    
+def main():
     pygame.init()
 
     pygame.display.set_caption("Conway's Game of Life")
@@ -12,17 +19,16 @@ def main():
     background.fill(pygame.Color(constants.BACKGROUND_COLOR))
     
     is_running = True
-
     while is_running:
-    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
     
         window_surface.blit(background, (0, 0))
-    
+        draw_grid(window_surface)
         pygame.display.update()
- 
+        
+
     
 if __name__ == "__main__":
     main()
