8ef8b21297254f50db4edff872fc9d9fdf94494d
Moises Angeles <moises.ea.camilo@gmail.com>
2026-02-25 00:13:39 -0500

    added game stages logic, play/pause button
    Next thing I have to do is figure out how to make it executable for others and how to make it run every 10 fps withou having to sacrifice the programs performance

--- cell.py
+++ cell.py
@@ -1,36 +1,28 @@
 
 class Cell:
     def __init__(self):
-        
-        self.x = None
-        self.y = None
+
         self.is_alive = False
         self.neighbour_count = None
         self.next_state = None
-    
-    def assign_pos(self,x,y):
-        self.x = x
-        self.y = y
-    
-    def prepare_next_gen(self, cell):
-        if cell.is_alive:
-            if cell.neighbour_count < 2:
-                cell.next_state = False
-                return
-            elif cell.neighbour_count > 3:
-                cell.next_state = False
+
+    def prepare_next_gen(self,):
+        if self.is_alive:
+            if self.neighbour_count == 2 or self.neighbour_count == 3:
+                self.next_state = True
                 return
             else:
-                cell.next_state = False
+                self.next_state = False
                 return
         else:
-            if cell.neighbour_count == 3:
-                cell.next_state = True
+            if self.neighbour_count == 3:
+                self.next_state = True
                 return
-            cell.next_state = False
+            self.next_state = False
             return 
         
                 
     def update(self): 
-        pass
-
+        if self.next_state is not None:
+            self.is_alive = self.next_state
+            self.next_state = None
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def draw_grid(surface):
     for y in range(0, HEIGHT, CELL_SIZE):
         pygame.draw.line(surface, GRID_LINE_COLOR, (0, y), (WIDTH, y), 2)
 
-def count_neighbours(board, cell):
+def count_neighbours(board, cell, x, y):
     coordinate_offset = [[-1,-1],[0,-1],[+1,-1],[-1,0],
                          [+1,0],[-1,+1],[0,+1],[+1,+1]]
     count = 0
@@ -19,15 +19,14 @@ def count_neighbours(board, cell):
         offset_x = coor[0]
         
         
-        if cell.y + (offset_y) < 0 or cell.x + (offset_x) < 0:
+        if y + (offset_y) < 0 or x + (offset_x) < 0:
             continue
-        if cell.y + (offset_y) >= ROWS or cell.x + (offset_x) >= COLS: 
+        if y + (offset_y) >= ROWS or x + (offset_x) >= COLS: 
             continue
         else:
-            x = cell.x + (offset_x)
-            y = cell.y + (offset_y)
-            print(f"Y:{y} X:{x}")
-            if board[y][x].is_alive:
+            ix = x + (offset_x)
+            iy = y + (offset_y) 
+            if board[iy][ix].is_alive:
                 
                 count += 1
     cell.neighbour_count = count
@@ -45,26 +44,43 @@ def main():
     board = [[Cell() for _ in range(COLS)] for _ in range(ROWS)]
     clock = pygame.time.Clock()
 
+    is_paused = True
     is_running = True
     while is_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_running = False
             
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    is_paused = not is_paused
+                
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     row = event.pos[1] // CELL_SIZE
                     col = event.pos[0] // CELL_SIZE
                     board[row][col].is_alive = not board[row][col].is_alive
-                
-                
+            
+     
+        if not is_paused:
+        # Esto es la parte de preparacion PHASE 1:         
+            for y, row in enumerate(board):
+                for x, cell in enumerate(row):
+                    count_neighbours(board, cell, x, y)
+                    cell.prepare_next_gen()
+        
+        #Esta se supone que es donde se actualize PHASE 2:
+            for row in board:
+                for cell in row:
+                    cell.update()
+        
+        #Esta se supone que es ahora donde se dibuja lo que ya se calculo PHASE 3:
         for iy, row_of_cells in enumerate(board):
             for ix, cell in enumerate(row_of_cells):
-                cell.assign_pos(ix,iy)
                 color = ((LIVE_CELL_COLOR) if cell.is_alive else (BACKGROUND_COLOR))
                 pygame.draw.rect(window_surface,color, (ix*CELL_SIZE, iy*CELL_SIZE, CELL_SIZE, CELL_SIZE) )
-                count_neighbours(board, cell)
-                
+       
+                    
                 
         draw_grid(window_surface)
         pygame.display.update()
